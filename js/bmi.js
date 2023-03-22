2c250f556e55cf16e658d7cca42cbf38698194b8
allen2352 <66169030+allen2352@users.noreply.github.com>
2023-03-22 18:09:48 +0800

    Delete js directory

--- js/bmi.js
+++
@@ -1,9 +0,0 @@
-$(() => {
-    $('button').on('click', () => {
-        let h = +$('#inputHeight').val()
-        let w = +$('#inputWeight').val()
-        let hh = h / 100
-        let bmi = w / (hh * hh)
-        $('#outputBMI').val(bmi)
-    })
-})
